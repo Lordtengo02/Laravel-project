03c98b69490b00349b90272b3113e41bb26340b2
LordTengo <23610336@utgz.edu.mx>
2025-07-05 18:51:35 -0600

    Proyecto inicial Tienda mx

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('lastOne');
-});
-
-Route::get('/bienvenidos', function () {
-    echo "Esto es una ruta de bienvenida";
     return view('welcome');
 });
-
-
-// Route::get('/contacto/formulario', function () { return view('contacto');
-// }) ->name('contacto');
-
-Route::get('/contacto/formulario',[App\Http\Controllers\ProductController::class,'index'])->name('contacto');
-
